6a118888e8e06b87003046679846eee0acf7bf6d
anubhavb1 <anubhav.bohidar82@gmail.com>
2016-02-13 20:00:19 +0530

    Initial commit

---
+++ app/src/main/java/gdgstudyjam/assignment1/MainActivity.java
@@ -0,0 +1,45 @@
+package gdgstudyjam.assignment1;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+//package com.example.android.gdg_cust_listview;
+
+public class MainActivity extends ListActivity {
+
+    String[] rappers = {
+            "Eminem",
+            "Dr. Dre",
+            "Ice Cube",
+            "Eazy e",
+            "2pac"
+    };
+    String[] descb={
+            "The Rap God",
+            "The Gangster",
+            "The man with the words",
+            "The blessed one",
+            "The swag man"
+    };
+    Integer[] imageIDs = {
+            R.drawable.eminem,
+            R.drawable.dre,
+            R.drawable.cube,
+            R.drawable.easy,
+            R.drawable.pac,
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        AdvancedCustomArrayAdapter adapter = new AdvancedCustomArrayAdapter(this,rappers,imageIDs,descb);
+        setListAdapter(adapter);
+
+    }
+
+
+
+}
